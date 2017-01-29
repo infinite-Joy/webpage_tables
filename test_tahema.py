52fb7b4ca27bc575a123c313da7d1e2bf2075621
joydeep bhattacharjee <joydeepubuntu@gmail.com>
2017-01-29 14:11:48 +0530

    fixed test case test_tabulate_data

--- test_tahema.py
+++ test_tahema.py
@@ -24,10 +24,10 @@ class CrawlerTest(unittest.TestCase):
             try:
                 if item[2] == 'DEED OF TRUST':
                     assertTrue(isinstance(item[0], tuple))
-                except IndexError:
-                    # handling this in case of item not being build.
-                    # we only want to see if there is atleast one valid item
-                    pass
+            except IndexError as e:
+                # handling this in case of item not being build.
+                # we only want to see if there is atleast one valid item
+                pass
 
         with Tahema(website) as t:
             for item in t._get_all_table_elements():
@@ -36,15 +36,17 @@ class CrawlerTest(unittest.TestCase):
 
     def test_tabulate_data(self):
         website = "./tahema_county_table_data.html"
-        t = Tahema(website)
-        tops = list(itertools.islice(t.tabulate_data(), 5))
-        self.assertTrue(tops[0] == ['VIEW', '2016014255', 'DEED OF TRUST', '12/01/2016', 'MILLER, EVELYN'])
+        with Tahema(website) as t:
+            tops = list(itertools.islice(t.tabulate_data(), 5))
+            self.assertEqual(tops[0][0][1], "VIEW", 'view element and view text')
 
     def test_check_instrument_type(self):
         row = ['VIEW', '2016014255', 'DEED OF TRUST', '12/01/2016', 'MILLER, EVELYN']
-        assert Tahema.check_instrument_type(row)
+        self.assert_(Tahema.check_instrument_type(row),
+            'row has a deed of trust')
         row = [1, 2, 'DEED', 4, 5]
-        self.assertTrue(Tahema.check_instrument_type(row))
+        self.assert_(Tahema.check_instrument_type(row),
+            'row has check instrument type deed')
 
 
 
